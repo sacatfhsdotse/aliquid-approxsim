fbf14e337eadae1a5fdbf5420b99c7050d34cc2e
jesajx <johannes.olegard@olissoft.com>
2014-02-01 18:07:37 +0100

    Minor fix.

--- StratmasServer/src/Subscription.cpp
+++ StratmasServer/src/Subscription.cpp
@@ -118,7 +118,7 @@ LayerSubscription::LayerSubscription(DOMElement* n, Buffer& buf, bool sbe)
 
      DOMElement* elem = XMLHelper::getFirstChildByTag(*n, "index");
      if (elem) {
-	  XMLByte* data = Base64::decodeToXMLByte(XMLHelper::getXMLChString(*n, "index"), (XMLSize_t*)&mLength);
+	  XMLByte* data = Base64::decode((const XMLByte * const)XMLHelper::getXMLChString(*n, "index"), (XMLSize_t*)&mLength);
 	  mIndex = reinterpret_cast<int32_t*>(data);
      }
      else {

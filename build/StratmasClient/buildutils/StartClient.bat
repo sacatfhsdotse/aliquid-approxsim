311c45cd80dda17a884f0a7fc6b202313a4ac179
jesajx <johannes.olegard@olissoft.com>
2014-02-11 13:02:48 +0100

    Major clean of StratmasClient.
    Moved around folders: the client's src, build, dist and install
    are nolonger so mixed up.
        src:     build/StratmasClient -> StratmasClient/src
        build:   build/StratmasClient/build -> StratmasClient/build
        dist:    build/StratmasClient/dist -> StratmasClient/dist
        install: . -> StratmasClient/install
    Removed make buildsystem for Client.
    Fixed ant buildsystem to accomodate changes.
    Renamed old doc.
    Set gitignore to ignore build, dist and install. Before these where being commited.

--- build/StratmasClient/buildutils/StartClient.bat
+++
@@ -1,6 +0,0 @@
-REM Batch file for starting the Stratmas client.
-
-@echo off
-
-REM Start client
-java -Xmx300m -Djava.library.path=StratmasClient\lib\Windows-x86 -cp StratmasClient StratmasClient.Client -noJoglResolve %1

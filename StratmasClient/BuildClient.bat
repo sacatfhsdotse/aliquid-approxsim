1e9b8574773710794ea5e67d9cf60b5db88fb9c6
jesajx@acheron <johannes.olegard@olissoft.com>
2014-02-18 21:19:09 +0100

    Deleted deprecated buildscripts.

--- StratmasClient/BuildClient.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Builds the Stratmas Client and places the installation in ..\..\..\StratmasClient
-
-set CLIENTDIR=..\..\StratmasClient
-
-@ant "-DinstallPrefix=..\%CLIENTDIR%" -buildfile "%CLIENTDIR%/build/StratmasClient/build.xml" install

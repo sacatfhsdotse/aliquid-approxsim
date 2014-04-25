49930a3aa7290c465b44f94f2de1233e1d66c190
jesajx <johannes.olegard@olissoft.com>
2014-04-25 17:58:17 +0200

    Fixed KTH shp-file some more.
    A shapefile records its own size (for some reason),
    and this is now taken into consideration when filtering.

--- StratmasClient/buildutils/shputil/shp.py
+++ StratmasClient/buildutils/shputil/shp.py
@@ -3,8 +3,39 @@
 import array
 import struct
 import sys
+import io
 
 defaultbyteorder="little"
+
+def putInt(f, v, byteorder=defaultbyteorder):
+    if byteorder == "little":
+        fmt = "<i"
+    elif byteorder == "big":
+        fmt = ">i"
+    else:
+        raise Exception("bad endian")
+    return f.write(struct.pack(fmt, v))
+
+def putInts(f, vs, byteorder=defaultbyteorder):
+    if byteorder == "little":
+        fmt = "<i"
+    elif byteorder == "big":
+        fmt = ">i"
+    else:
+        raise Exception("bad endian")
+    for v in vs:
+        f.write(struct.pack(fmt, v))
+        
+
+def putDoubles(f, vs, byteorder=defaultbyteorder):
+    if byteorder == "little":
+        fmt = "<d"
+    elif byteorder == "big":
+        fmt = ">d"
+    else:
+        raise Exception("bad endian")
+    for v in vs:
+        f.write(struct.pack(fmt, v))
 
 def getInt(f, byteorder=defaultbyteorder, signed=True):
     return int.from_bytes(f.read(4), byteorder=byteorder, signed=signed)
@@ -84,7 +115,7 @@ def getPolygonZ(f, rlen):
 #def getMultiPointM(f, rlen):
 #def getMultiPatch(f, rlen):
 
-rtypes = {
+rtypes_gets = {
         0 : getNull,
         1 : getPoint,
         3 : getPolyline,
@@ -102,27 +133,147 @@ rtypes = {
 }
 
 def getType(f, rtype, rlen):
-    return rtypes[rtype](f, rlen)
+    return rtypes_gets[rtype](f, rlen)
 
-def printShpfile(dafile):
-    with open(dafile, "rb") as f:
-        fcode = getInt(f, byteorder="big")
-        funused = getInts(f, 5, byteorder="big")
-        flen = getInt(f, byteorder="big")
-        fver = getInt(f, byteorder="little")
-        fshptype = getInt(f, byteorder="little")
-        print("fcode =", hex(fcode))
-        print("funused =", funused)
-        print("flen =", flen)
-        print("fver =", fver)
-        print("fshptype =", fshptype)
+def putPoint(f, rdata):
+    putDoubles(f, list(rdata))
+    return 2*8
 
-        fmbr = getDoubles(f, 4)
-        fZr = getDoubles(f, 2)
-        fMr = getDoubles(f, 2);
-        print("fmbr =", fmbr)
-        print("fZr =", fZr)
-        print("fMr =", fMr)
+def putPolygon(f, rdata):
+    box = rdata[0]
+    numParts = rdata[1]
+    numPoints = rdata[2]
+    parts = rdata[3]
+    points = rdata[4]
+
+    putDoubles(f, box)
+    putInt(f, numParts)
+    putInt(f, numPoints)
+    putInts(f, parts)
+    plen = 0
+    for p in points:
+        plen = plen + putPoint(f, p)
+    return 4*8 + 4 + 4 + 4*len(parts) + plen
+
+rtypes_puts = {
+        #0 : putNull,
+        1 : putPoint,
+        #3 : putPolyline,
+        5 : putPolygon,
+        #8 : putMultiPoint,
+        #11: putPointZ,
+        #13: putPolylineZ,
+        #15: putPolygonZ,
+        #18: putMultiPointZ,
+        #21: putPointM,
+        #23: putPolylineM,
+        #25: putPolygonM,
+        #28: putMultiPointM,
+        #31: putMultiPatch
+}
+
+
+def putType(f, rtype, rdata):
+    return rtypes_puts[rtype](f, rdata)
+
+def getShpHeader(f):
+    return {
+        "fcode" : getInt(f, byteorder="big"),
+        "funused" : getInts(f, 5, byteorder="big"),
+        "flen" : getInt(f, byteorder="big"),
+        "fver" : getInt(f, byteorder="little"),
+        "fshptype" : getInt(f, byteorder="little"),
+        "fmbr" : getDoubles(f, 4),
+        "fZr" : getDoubles(f, 2),
+        "fMr" : getDoubles(f, 2)
+    }
+
+def printShpHeader(header):
+    print("fcode =", header["fcode"])
+    print("funused =", header["funused"])
+    print("flen =", header["flen"])
+    print("fver =", header["fver"])
+    print("fshptype =", header["fshptype"])
+    print("fmbr=", header["fmbr"])
+    print("fZr=", header["fZr"])
+    print("fMr=", header["fMr"])
+
+def filterShpFile(infile, outfile):
+    with open(outfile, "wb") as o:
+        with open(infile, "rb") as f:
+            header = getShpHeader(f)
+            printShpHeader(header)
+
+            if header["fshptype"] == 15: # PolygonZ
+                header["fshptype"] = 5 # Polygon (non-Z)
+
+            putInt(o, header["fcode"], byteorder="big")
+            putInts(o, header["funused"], byteorder="big")
+            putInt(o, header["flen"], byteorder="big")
+            putInt(o, header["fver"], byteorder="little")
+            putInt(o, header["fshptype"], byteorder="little")
+            putDoubles(o, header["fmbr"])
+            putDoubles(o, header["fZr"])
+            putDoubles(o, header["fMr"])
+
+            flen = header["flen"]
+
+            nlen = 50
+
+            hlen = 100
+            flen = flen * 2 # 16-bit words to 8-bit words
+            flen = flen - hlen
+
+            while flen > 0:
+                print() # separator
+
+                rno = getInt(f, byteorder="big")
+                rlen = getInt(f, byteorder="big")
+                print("rno =", rno)
+                print("rlen =", rlen)
+
+                rtype = getInt(f, byteorder="little")
+                print("rtype =", rtype)
+
+                if rtype not in rtypes_gets:
+                    print("unimplemented type ==> dying")
+                    sys.exit(1)
+
+                rdata = getType(f, rtype, rlen)
+                print("rdata =", rdata)
+
+                if rtype == 15: # PolygonZ
+                    rtype = 5 # Polygon  (no Z)
+                    rdata = tuple(rdata[0:5])
+
+                putInt(o, rno, byteorder="big")
+                putInt(o, rlen, byteorder="big")
+                putInt(o, rtype, byteorder="little")
+
+                nrlen = 4+putType(o, rtype, rdata)
+                print("nrlen =", nrlen)
+
+                o.seek(-nrlen+4, 1)
+                putInt(o, nrlen, byteorder="big")
+                o.seek(-4+nrlen, 1)
+
+                flen = flen - 2*4 - 2*rlen
+                nlen = nlen + (2*4 + nrlen)/2
+
+            
+            nlen = int(nlen)
+            print("nlen =", nlen)
+            o.seek(24)
+            putInt(o, nlen, byteorder="big")
+
+def printShpFile(infile):
+    with open(infile, "rb") as f:
+        header = getShpHeader(f)
+        printShpHeader(header)
+
+        flen = header["flen"]
+
+        nlen = 50
 
         hlen = 100
         flen = flen * 2 # 16-bit words to 8-bit words
@@ -138,12 +289,20 @@ def printShpfile(dafile):
 
             rtype = getInt(f, byteorder="little")
             print("rtype =", rtype)
-            if rtype in rtypes:
-                rdata = getType(f, rtype, rlen)
-                print("rdata =", rdata)
-            else:
-                print("unimplemented type ==> skipping")
-                f.read(rlen*2)
-            flen = flen - 2*(2*2 + rlen)
 
-printShpfile(sys.argv[1])
+            if rtype not in rtypes_gets:
+                print("unimplemented type ==> dying")
+                sys.exit(1)
+
+            rdata = getType(f, rtype, rlen)
+            print("rdata =", rdata)
+
+            flen = flen - 2*4 - 2*rlen
+
+
+if len(sys.argv) == 3:
+    filterShpFile(sys.argv[1], sys.argv[2])
+elif len(sys.argv) == 2:
+    printShpFile(sys.argv[1])
+else:
+    print("Wrong no of args.")

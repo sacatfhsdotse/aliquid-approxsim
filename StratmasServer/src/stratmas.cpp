341cb9c361a5e38f9e58a25aa4db3f12934d46c3
pintjuk <danielpintjuk@gmail.com>
2014-04-27 00:06:29 +0200

    rebranded

--- StratmasServer/src/stratmas.cpp
+++ StratmasServer/src/stratmas.cpp
@@ -46,7 +46,7 @@ int main(int argc, char **argv)
 {
      // Note that Environment::initEnvironment(argc, argv); is
      // expected to be called first in main.
-     std::cout << "\n    S T R A T M A S\n    -server \n\n";
+     std::cout << "\n    A P P R O X I M A T E\n    -Crisis Sim\n\n";
      Environment::initEnvironment(argc, argv);
 
      // Initialize logging

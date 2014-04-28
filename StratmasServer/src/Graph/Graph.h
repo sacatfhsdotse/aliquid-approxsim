19a2a7aeb1c4d093e79cba5eacd1141802732f3d
pintjuk <danielpintjuk@gmail.com>
2014-04-28 03:44:50 +0200

    graph classes

--- StratmasServer/src/Graph/Graph.h
+++ StratmasServer/src/Graph/Graph.h
@@ -1,17 +1,80 @@
 #ifndef STRATMAS_GRAPH_H
 #define STRATMAS_GRAPH_H
 
+
+#include <string>
+#include <LatLng.h>
+
+/**
+ * \brief This class represents a Node.
+ *
+ * \author   Daniil Pintjuk
+ * \date     $Date: 2014/04/25 20:14:00 $
+ */
+class Node
+{
+protected:
+	LatLng pos;
+public:
+	Node(LatLng p):pos(p){
+
+	}
+	Node(){
+
+	}
+};
+
+/**
+ * \brief This class represents a Edg.
+ *
+ * \author    Daniil Pintjuk
+ * \date     $Date: 2014/04/25 20:14:00 $
+ */
+class Edge
+{
+protected:
+	int origin;
+	int target;
+	bool conected;
+	double travalspeed;
+public:
+	Edge(int o, int t, bool con, double tsped):
+		origin(o), target(t), conected(con), travalspeed(tsped){
+
+	}
+	Edge(){
+
+	}
+};
+
+
+
 /**
  * \brief This class represents a Graph.
  *
- * \author   Johannesd Olegård
+ * \author   Johannesd Olegård, Daniil Pintjuk
  * \date     $Date: 2014/04/25 20:14:00 $
  */
 class Graph
 {
-private:
+protected:
+	int numNodes;
+	Node* nodes;
+	int numEdges;
+	Edge* edges;
 public:
+	Graph(
+		int NumNodes,
+		Node* Nodes,
+		int NumEdges,
+		Edge* Edges):
+		numNodes(NumNodes),
+		nodes(Nodes),
+		numEdges(NumEdges),
+		edges(Edges){}
 };
+
+
 
 
 #endif   // STRATMAS_GRAPH_H
